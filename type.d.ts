74b495e2f2c6edb26186e62f51b1ad05cddbf628
Palwasheyyy <163187583+Palwasha-48@users.noreply.github.com>
2025-02-04 20:53:33 +0500

    Update type.d.ts

--- type.d.ts
+++ type.d.ts
@@ -1,9 +1,15 @@
+import { internalGroqTypeReferenceTo, SanityImageCrop, SanityImageHotspot, Slug } from "./sanity.types";
 
-interface Product {
+type Product = {
+    image(image: any): unknown;
+    id: Key | null | undefined;
+    category: ReactI18NextChildren | Iterable<ReactI18NextChildren>;
+    name: string;
     map(arg0: (product: { imageUrl: string | import("next/dist/shared/lib/get-img-props").StaticImport; title: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | Promise<React.AwaitedReactNode> | null | undefined; price: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | Iterable<React.ReactNode> | null | undefined; }, index: React.Key | null | undefined) => React.JSX.Element): React.ReactNode | Iterable<React.ReactNode>;
     discount: ReactI18NextChildren | Iterable<ReactI18NextChildren>;
     _id: string;
     title: string;
+    slug?: Slug;
     price: number;
     description: string;
     dicountParcentage: number;
